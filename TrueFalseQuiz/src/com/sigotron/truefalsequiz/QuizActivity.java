7574a826ecba4a1a71bc852f55bc97d4ee62b5f6
Sigotron <sigotron@gmail.com>
2014-03-26 12:09:06 -0700

    First Commit

---
+++ TrueFalseQuiz/src/com/sigotron/truefalsequiz/QuizActivity.java
@@ -0,0 +1,88 @@
+package com.sigotron.truefalsequiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity {
+	
+	// member variables
+	private Button mTrueButton; // the m is an android naming convention
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_dogs, true),
+			new TrueFalse(R.string.question_mother, true),
+			new TrueFalse(R.string.question_you, false),
+			new TrueFalse(R.string.question_joel, true),
+			new TrueFalse(R.string.question_bears, true),
+			};
+	private int mCurrentIndex = 0;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		
+		// wire up the TextView
+		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+		updateQuestion();
+			
+		// get reference to the button widgets
+		// returns a View which I must cast to a Button
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		
+		// set up listeners
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkAnswer(true);
+			}
+		});
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+		});
+		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+	}
+
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int messageResId = 0;
+		if (userPressedTrue == answerIsTrue) {
+			messageResId = R.string.correct_toast;
+		} else {
+			messageResId = R.string.incorrect_toast;
+		}
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
+
+}
